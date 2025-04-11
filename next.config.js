dd04f7b0ae217343e8b17be6a527c022f8d91f38
Zenslim <zenslim2010@gmail.com>
2025-04-11 18:47:27 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,19 @@
-module.exports = { reactStrictMode: true }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  async rewrites() {
+    return [
+      {
+        source: '/signin',
+        destination: '/signin',
+      },
+    ];
+  },
+
+  // Optional but helpful:
+  // Ensures consistent URL behavior
+  trailingSlash: false,
+};
+
+module.exports = nextConfig;
