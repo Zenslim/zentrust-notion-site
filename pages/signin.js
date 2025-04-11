5b90e73723aa3291155ba8e532c0ad5e34216d7b
Zenslim <zenslim2010@gmail.com>
2025-04-11 17:22:30 -0400

    Re-add real signin.js with working Google login

---
+++ pages/signin.js
@@ -0,0 +1,11 @@
+// pages/signin.js
+import SignIn from '../components/SignIn';
+
+export default function SignInPage() {
+  return (
+    <main className="min-h-screen flex flex-col justify-center items-center bg-black text-white">
+      <h1 className="text-3xl font-bold mb-6">Welcome to ZenTrust 🧘</h1>
+      <SignIn />
+    </main>
+  );
+}
