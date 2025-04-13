a12ce3894307a60d08e2233465928f30157e80a6
Zenslim <zenslim2010@gmail.com>
2025-04-13 13:10:08 -0400

    fix: add <link rel="manifest"> in _document.js to resolve 404

--- pages/_document.js
+++ pages/_document.js
@@ -1,26 +1,19 @@
-// pages/_document.js
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Favicon */}
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="192x192" href="/favicon-192x192.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="/favicon-512x512.png" />
-
-        {/* Manifest (optional for PWA) */}
         <link rel="manifest" href="/manifest.json" />
-
-        {/* Theme color */}
-        <meta name="theme-color" content="#ffffff" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <meta name="theme-color" content="#000000" />
       </Head>
-      <body>
+      <body className="bg-black text-white">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
-
