37cc0c736d2e520b6acd39eaa3e9a04507c56f39
Zenslim <zenslim2010@gmail.com>
2025-04-12 20:47:48 -0400

    Update [slug].js

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,52 +1,71 @@
-// pages/[slug].js
-import React from 'react'
+import { NotionAPI } from 'notion-client'
 import { NotionRenderer } from 'react-notion-x'
 import dynamic from 'next/dynamic'
-import { NotionAPI } from 'notion-client'
 import 'react-notion-x/src/styles.css'
-import 'prismjs/themes/prism-tomorrow.css'
 
-const Code = dynamic(() => import('react-notion-x/build/third-party/code'))
-const Collection = dynamic(() => import('react-notion-x/build/third-party/collection'))
-const Equation = dynamic(() => import('react-notion-x/build/third-party/equation'))
-const Pdf = dynamic(() => import('react-notion-x/build/third-party/pdf'))
-const Modal = dynamic(() => import('react-notion-x/build/third-party/modal'))
+const Code = dynamic(() =>
+  import('react-notion-x/build/third-party/code').then(m => m.Code)
+)
+const Collection = dynamic(() =>
+  import('react-notion-x/build/third-party/collection').then(m => m.Collection)
+)
+const Equation = dynamic(() =>
+  import('react-notion-x/build/third-party/equation').then(m => m.Equation)
+)
+const Pdf = dynamic(() =>
+  import('react-notion-x/build/third-party/pdf').then(m => m.Pdf)
+)
+const Modal = dynamic(() =>
+  import('react-notion-x/build/third-party/modal').then(m => m.Modal),
+  { ssr: false }
+)
 
-const pageMap = {
- why: '1d17b43e0a0c80caa37ff56c1c9943be',  // WHY - Why We Exist
-  how: '1d17b43e0a0c805a9676ec69cb7a5bf2',  // HOW - The ZenTrust Way
-  what: '1d07b43e0a0c80448e3fd83b044656a8'  // WHAT WE OFFER
-}
-
-const Page = ({ recordMap }) => {
-  return (
-    <div className="max-w-4xl mx-auto py-12 px-4">
-      <NotionRenderer
-        recordMap={recordMap}
-        components={{ Code, Collection, Equation, Pdf, Modal }}
-        fullPage={true}
-        darkMode={false}
-      />
-    </div>
-  )
+const notionPageIds = {
+  why: '1d17b43e0a0c80caa37ff56c1c9943be',
+  how: '1d17b43e0a0c805a9676ec69cb7a5bf2',
+  what: '1d07b43e0a0c80448e3fd83b044656a8'
 }
 
 export async function getStaticPaths() {
+  const paths = Object.keys(notionPageIds).map(slug => ({
+    params: { slug }
+  }))
   return {
-    paths: Object.keys(pageMap).map((slug) => ({
-      params: { slug }
-    })),
+    paths,
     fallback: false
   }
 }
 
 export async function getStaticProps({ params }) {
-  const api = new NotionAPI()
-  const recordMap = await api.getPage(pageMap[params.slug])
+  const id = notionPageIds[params.slug]
+  const notion = new NotionAPI()
+  const recordMap = await notion.getPage(id)
+
   return {
-    props: { recordMap },
-    revalidate: 60
+    props: {
+      recordMap
+    },
+    revalidate: 10
   }
 }
 
-export default Page
+export default function NotionPage({ recordMap }) {
+  return (
+    <div className="bg-white text-black min-h-screen p-4 md:p-12">
+      <NotionRenderer
+        recordMap={recordMap}
+        fullPage={true}
+        disableHeader={true}
+        header={null}
+        footer={null}
+        components={{
+          Code,
+          Collection,
+          Equation,
+          Pdf,
+          Modal
+        }}
+      />
+    </div>
+  )
+}
