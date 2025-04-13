8153a25ed7a97d38505af2cf84bd0d40319fe8bb
Zenslim <zenslim2010@gmail.com>
2025-04-13 15:03:50 -0400

    Fix: Add full firebase.js with all auth exports

--- firebase.js
+++ firebase.js
@@ -1,24 +1,12 @@
-// firebase.js
 import { initializeApp } from 'firebase/app';
-import {
-  getAuth,
-  isSignInWithEmailLink,
-  signInWithEmailLink,
-  sendSignInLinkToEmail,
-  signInWithPopup,
-  signInAnonymously,
-  GoogleAuthProvider,
-  GithubAuthProvider,
-  FacebookAuthProvider,
-  TwitterAuthProvider,
-} from 'firebase/auth';
+import { getAuth, isSignInWithEmailLink, sendSignInLinkToEmail, signInWithEmailLink, signInWithPopup, signInAnonymously, GoogleAuthProvider, GithubAuthProvider, FacebookAuthProvider, TwitterAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_DOMAIN',
+  authDomain: 'YOUR_AUTH_DOMAIN',
   projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_BUCKET',
+  storageBucket: 'YOUR_STORAGE_BUCKET',
   messagingSenderId: 'YOUR_SENDER_ID',
   appId: 'YOUR_APP_ID',
 };
@@ -27,7 +15,6 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// Auth Providers
 const googleProvider = new GoogleAuthProvider();
 const githubProvider = new GithubAuthProvider();
 const facebookProvider = new FacebookAuthProvider();
@@ -37,14 +24,11 @@ export {
   app,
   auth,
   db,
-  // Email Magic Link
   isSignInWithEmailLink,
-  signInWithEmailLink,
   sendSignInLinkToEmail,
-  // Other Auth Methods
+  signInWithEmailLink,
   signInWithPopup,
   signInAnonymously,
-  // Providers
   googleProvider,
   githubProvider,
   facebookProvider,
