d3256f131e97ca7b4c74c4fb71fea3c5965b6a1f
Zenslim <zenslim2010@gmail.com>
2025-04-13 14:41:25 -0400

    fix: add Firestore and Auth to Firebase config

--- firebase.js
+++ firebase.js
@@ -1,4 +1,8 @@
+// firebase.js
+
 import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: 'YOUR_API_KEY',
@@ -9,4 +13,8 @@ const firebaseConfig = {
   appId: 'YOUR_APP_ID',
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { app, db, auth };
