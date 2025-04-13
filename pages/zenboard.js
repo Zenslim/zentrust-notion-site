259fd05b36a03cb917c463ff5f963e59e320d367
Zenslim <zenslim2010@gmail.com>
2025-04-13 13:59:13 -0400

    Fix: BPSS compass layout centered with radial symmetry

--- pages/zenboard.js
+++ pages/zenboard.js
@@ -15,24 +15,28 @@ export default function Zenboard() {
     <div className="flex flex-col items-center justify-center min-h-screen py-12 bg-gradient-to-br from-gray-900 via-black to-gray-800 text-white relative overflow-hidden">
 
       {/* Glowing Background Orb */}
-      <div className="absolute w-[400px] h-[400px] bg-purple-700 rounded-full blur-3xl opacity-20 animate-pulse -z-10 top-24 left-1/2 transform -translate-x-1/2" />
+      <div className="absolute w-[500px] h-[500px] bg-purple-700 rounded-full blur-3xl opacity-20 animate-pulse -z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
 
-      {/* BPSS Compass */}
-      <div className="relative w-[300px] h-[300px] grid place-items-center">
+      {/* Compass Layout */}
+      <div className="relative w-[320px] h-[320px]">
         <PulseRing bp={bp} />
-        <div className="absolute top-0">
+
+        <div className="absolute top-0 left-1/2 transform -translate-x-1/2">
           <NudgeArc direction="top" label="Spiritual" prompt="What brings meaning?" level={bp.spiritual} />
         </div>
-        <div className="absolute bottom-0">
+        <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2">
           <NudgeArc direction="bottom" label="Psycho" prompt="What sparks your mind?" level={bp.psycho} />
         </div>
-        <div className="absolute left-0">
+        <div className="absolute left-0 top-1/2 transform -translate-y-1/2">
           <NudgeArc direction="left" label="Bio" prompt="What energizes your body?" level={bp.bio} />
         </div>
-        <div className="absolute right-0">
+        <div className="absolute right-0 top-1/2 transform -translate-y-1/2">
           <NudgeArc direction="right" label="Social" prompt="Who needs your presence?" level={bp.social} />
         </div>
-        <IkigaiCenter ikigai={ikigai} />
+
+        <div className="absolute inset-0 flex items-center justify-center">
+          <IkigaiCenter ikigai={ikigai} />
+        </div>
       </div>
 
       <div className="mt-12">
