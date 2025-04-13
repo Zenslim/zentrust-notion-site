0bdb3b5b1df6b3df33e453a93ff848bfa1345662
Zenslim <zenslim2010@gmail.com>
2025-04-13 14:45:29 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,19 @@
 // firebase.js
-
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_DOMAIN',
+  authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
   projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_BUCKET',
+  storageBucket: 'YOUR_PROJECT_ID.appspot.com',
   messagingSenderId: 'YOUR_SENDER_ID',
   appId: 'YOUR_APP_ID',
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { app, db, auth };
+export { app, auth, db };
