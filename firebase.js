c8b2a78da1ee1710ebdf13cdf6f5b9774ec70040
Zenslim <zenslim2010@gmail.com>
2025-04-13 14:50:44 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,52 @@
 // firebase.js
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import {
+  getAuth,
+  isSignInWithEmailLink,
+  signInWithEmailLink,
+  sendSignInLinkToEmail,
+  signInWithPopup,
+  signInAnonymously,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  FacebookAuthProvider,
+  TwitterAuthProvider,
+} from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD5...real_key',
-  authDomain: 'zentrust-e647d.firebaseapp.com',
-  projectId: 'zentrust-e647d',
-  storageBucket: 'zentrust-e647d.appspot.com',
-  messagingSenderId: '7350...',
-  appId: '1:7350...:web:abc123def456',
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_DOMAIN',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_BUCKET',
+  messagingSenderId: 'YOUR_SENDER_ID',
+  appId: 'YOUR_APP_ID',
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { app, auth, db };
+// Auth Providers
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
+const twitterProvider = new TwitterAuthProvider();
+
+export {
+  app,
+  auth,
+  db,
+  // Email Magic Link
+  isSignInWithEmailLink,
+  signInWithEmailLink,
+  sendSignInLinkToEmail,
+  // Other Auth Methods
+  signInWithPopup,
+  signInAnonymously,
+  // Providers
+  googleProvider,
+  githubProvider,
+  facebookProvider,
+  twitterProvider,
+};
