64b6dce48d917155aae0dc9a77eba62c2f868d50
Zenslim <zenslim2010@gmail.com>
2025-04-14 13:30:16 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
+// pages/_app.jsx
 import '../styles/globals.css'
+import { useEffect } from 'react'
+import { auth } from '../firebase'
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        console.log("✅ Signed in as:", user.email || "Anonymous", "| UID:", user.uid)
+      } else {
+        console.warn("⚠️ No user signed in.")
+      }
+    })
+    return () => unsubscribe()
+  }, [])
+
   return <Component {...pageProps} />
 }
