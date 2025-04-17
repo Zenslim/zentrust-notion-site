f6dc0a30f02f1fde0d3008f9cb8643fa535345c0
Zenslim <zenslim2010@gmail.com>
2025-04-17 10:49:32 -0400

    Update BreathingOrb.jsx

--- components/ZenJoystick/BreathingOrb.jsx
+++ components/ZenJoystick/BreathingOrb.jsx
@@ -1,7 +1,5 @@
-// File: components/ZenJoystick/BreathingOrb.jsx
-
 export default function BreathingOrb() {
   return (
-    <div className="absolute top-1/2 left-1/2 w-[500px] h-[500px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-purple-500/30 blur-[120px] animate-zenpulse z-0" />
+    <div className="absolute top-1/2 left-1/2 w-[500px] h-[500px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-purple-900 opacity-25 blur-3xl animate-zenpulse z-0" />
   );
 }
