7de1800e3788b261b936cbbefa413ef0b6d3d3f5
Zenslim <zenslim2010@gmail.com>
2025-04-17 10:18:43 -0400

    Update BreathingOrb.jsx

--- components/ZenJoystick/BreathingOrb.jsx
+++ components/ZenJoystick/BreathingOrb.jsx
@@ -1,7 +1,6 @@
-import React from "react"
+import React from "react";
+import styles from '../../styles/chakraGlow.module.css';
 
 export default function BreathingOrb() {
-  return (
-    <div className="absolute top-1/2 left-1/2 w-[500px] h-[500px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-purple-900 opacity-25 blur-3xl animate-pulse-slow" />
-  )
+  return <div className={styles.breathingOrb} />;
 }
