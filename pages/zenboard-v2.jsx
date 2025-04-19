7a6083900979570a439208d23e8ee97c481d6b28
Zenslim <zenslim2010@gmail.com>
2025-04-18 21:01:05 -0400

    Update zenboard-v2.jsx

--- pages/zenboard-v2.jsx
+++ pages/zenboard-v2.jsx
@@ -1,4 +1,8 @@
 import ChakraGlowV2 from '@/components/ChakraGlowV2';
+import CelestialBackground from './ZenJoystick/CelestialBackground';
+...
+<CelestialBackground />
+
 
 export default function ZenboardV2() {
   return <ChakraGlowV2 />;
