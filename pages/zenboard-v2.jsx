d83a0a7296d1cf89d73badd762ab37365a496159
Zenslim <zenslim2010@gmail.com>
2025-04-19 13:10:38 -0400

    Update zenboard-v2.jsx

--- pages/zenboard-v2.jsx
+++ pages/zenboard-v2.jsx
@@ -1,11 +1,12 @@
-
 import CelestialBackground from '@/components/CelestialBackground';
 import PlanetMessenger from '@/components/PlanetMessenger';
+import MoonSync from '@/components/MoonSync';
 
 export default function ZenboardV2() {
   return (
     <div className="relative w-full h-screen overflow-hidden bg-black">
       <CelestialBackground />
+      <MoonSync />
       <div className="absolute inset-0 flex items-center justify-center z-10">
         <PlanetMessenger />
       </div>
