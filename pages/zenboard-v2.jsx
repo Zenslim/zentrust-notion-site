009703871a7c5765c4cbcfcd51429c66d15b7caa
Zenslim <zenslim2010@gmail.com>
2025-04-18 21:02:26 -0400

    Update zenboard-v2.jsx

--- pages/zenboard-v2.jsx
+++ pages/zenboard-v2.jsx
@@ -1,9 +1,13 @@
 import ChakraGlowV2 from '@/components/ChakraGlowV2';
-import CelestialBackground from './ZenJoystick/CelestialBackground';
-...
-<CelestialBackground />
-
+import CelestialBackground from '@/components/ZenJoystick/CelestialBackground';
 
 export default function ZenboardV2() {
-  return <ChakraGlowV2 />;
+  return (
+    <div className="relative min-h-screen bg-black text-white overflow-hidden">
+      <CelestialBackground />
+      <div className="relative z-10 flex flex-col items-center justify-center min-h-screen">
+        <ChakraGlowV2 />
+      </div>
+    </div>
+  );
 }
