4a7e1ba98c4cf68c3f0d56a85ff679553bd964a3
Zenslim <zenslim2010@gmail.com>
2025-04-20 11:03:54 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 const path = require('path');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
 
-module.exports = {
+module.exports = withPWA({
   reactStrictMode: true,
   trailingSlash: false,
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
   },
-};
+});
